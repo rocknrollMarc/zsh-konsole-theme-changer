a8e1d938e3307f930ebdf7710ea3e14daf79f483
Grigorii Horos <horosgrisa@gmail.com>
2015-03-30 12:20:35 +0300

    Bugfix: Remove key

--- konsole-theme-changer.plugin.zsh
+++ konsole-theme-changer.plugin.zsh
@@ -2,20 +2,19 @@
 
 THEME_ONE="Solarized"
 THEME_TWO="SolarizedLight"
-CHANGE_THEME_KEY="^[[24~"
 
-_THEME=0;
+_KONSOLE_THEME=0;
 
 function change_theme(){
-	if [[ $_THEME == 0 ]]; then
-		_THEME=1
+	if [[ $_KONSOLE_THEME == 0 ]]; then
+		_KONSOLE_THEME=1
 		printf "\033]50;colors=%s\a" "$THEME_TWO"
 	else
-		_THEME=0
+		_KONSOLE_THEME=0
 		printf "\033]50;colors=%s\a" "$THEME_ONE"
 	fi
 }
 
 zle -N change_theme
 
-bindkey $CHANGE_THEME_KEY change_theme
+bindkey "^[[24~" change_theme
